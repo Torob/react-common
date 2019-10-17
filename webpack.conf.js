bc4c6b143a55f54d5f7cd5d52d495992d1e8fbad
n1rna <n7r.distributed.ai@gmail.com>
2019-10-17 13:57:50 +0330

    Change output path in webpack config file

--- webpack.conf.js
+++ webpack.conf.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
+    filename: 'index.js',
   },
   module: {
     rules: [
