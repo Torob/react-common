a206b53794956ca4a703e2522e3d894e13dc55bd
n1rna <n7r.distributed.ai@gmail.com>
2019-10-17 13:47:31 +0330

    Move babel configs to webpack and modify package.json

--- webpack.conf.js
+++ webpack.conf.js
@@ -12,7 +12,19 @@ module.exports = {
         exclude: path.resolve(__dirname, 'node_modules'),
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+            ],
+          },
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', { loader: 'css-loader', options: { import: true } }],
       },
     ],
   },
