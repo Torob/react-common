9f9d6fc7a9f3ea50bf3350d711212e9907f08c8d
AhmadF <fanaei.ahmad@gmail.com>
2019-10-18 17:02:04 +0330

    commit blog static content

--- src/components/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
-
-export default axios;
--- src/components/tags/baseProductFeaturesTag.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BaseProductFeaturesTag = () => '';
-
-export default BaseProductFeaturesTag;
