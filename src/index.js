f3ce80fb313a56be3222be2ae11a3207694825a7
n1rna <n7r.distributed.ai@gmail.com>
2019-10-30 16:51:22 +0330

    Try to fix unambiguous in babel rc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-'use strict';
+import TorobHeader from './components/TorobHeader/react-bootstrap_compatible';
+import axios from './components/axios';
+import ClickableText from './components/clickableText';
+import Divider from './components/divider';
+import { TitleComponent, withTitle } from './components/dynamicTitle';
+import MainNavigation from './components/mainNavigation';
+import SecondaryNavigation from './components/secondaryNavigation';
+import PrivateRoute from './components/privateRoute';
+import SearchFilters from './components/searchFilters';
+import SearchInfo from './components/searchInfo';
+import Toast from './components/toast';
+import FullscreenDialog from './components/fullscreenDialog';
+import TorobStyles from './components/torobStyles';
+import { DynamicCategorySelector, StaticCategorySelector } from './components/categorySelector';
+import { DynamicShopSelector, StaticShopSelector } from './components/shopSelector';
+import ImageHolder from './components/imageHolder';
+import { useNotif, NotificationProvider } from './hooks/useNotif';
+import useTorobAPI from './hooks/useTorobAPI';
+import { AuthProvider, useAuth } from './providers/auth';
 
-const { ALL } = require('./main');
+const DynamicTitle = { TitleComponent, withTitle };
+const hooks = { useNotif, useTorobAPI, useAuth };
+const Providers = { AuthProvider, NotificationProvider };
 
-// ALL.ALL = AL;
-module.exports = ALL;
+export {
+  TorobHeader,
+  axios,
+  ClickableText,
+  Divider,
+  DynamicTitle,
+  MainNavigation,
+  SecondaryNavigation,
+  PrivateRoute,
+  SearchFilters,
+  SearchInfo,
+  Toast,
+  FullscreenDialog,
+  TorobStyles,
+  DynamicCategorySelector,
+  StaticCategorySelector,
+  DynamicShopSelector,
+  StaticShopSelector,
+  ImageHolder,
+  Providers,
+  hooks,
+};
