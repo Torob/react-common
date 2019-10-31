27e0e299e3fbbce5b2cd202230fab5fa20d8e80d
n1rna <n7r.distributed.ai@gmail.com>
2019-10-31 17:33:11 +0330

    Renew scripts with rollup

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import json from 'rollup-plugin-json'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    json(),
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      plugins: [
+        "@babel/plugin-transform-runtime",
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ],
+      ignore: [
+        "node_modules/**"
+      ],
+      runtimeHelpers: true
+    }),
+    resolve({ preferBuiltins: true }),
+    commonjs({
+      // exclude: [
+      //   'node_modules/react-dom/**'
+      // ],
+      namedExports: {
+        // 'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+        'node_modules/react-dom/index.js': ['findDOMNode', 'createPortal'],
+        'node_modules/react-is/index.js': ['isValidElementType']
+      }
+    })
+  ]
+}
