45d96fa8a54e8eb73b638c6c2e5d4c75e4d93cdb
n1rna <n7r.distributed.ai@gmail.com>
2019-12-08 17:38:25 +0330

    Modify main navigation component and make some fixes

--- src/components/mainNavigation.js
+++ src/components/mainNavigation.js
@@ -1,45 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FiShoppingCart, FiMessageSquare, FiUser, FiGitMerge } from 'react-icons/fi';
+import { FiShoppingCart, FiMessageSquare, FiUser } from 'react-icons/fi';
 import { StyleSheet, css } from 'aphrodite';
-import { bgStyles } from './torobStyles';
+import { bgStyles, colors } from './torobStyles';
+import { CrowdSourceIcon } from './torobIcons';
 import urls from '../urls';
 import { Nav, Col, Row } from 'react-bootstrap';
 
-const MainNavigation = ({ activePane }) => (
-  <div className={css(styles.mainSidebarStick, bgStyles['bg-skylight'])}>
-    <Row>
-      <Col>
-        <Nav variant="pills" className="flex-column">
-          <Nav.Item className={css(styles.navItem)}>
-            <Nav.Link eventKey="first" active={activePane === 'shop-panel'} href={urls.shopPanel()}>
-              <FiShoppingCart />
-              فروشگاه
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className={css(styles.navItem)}>
-            <Nav.Link eventKey="second" active={activePane === 'crowdsource'} href={urls.crowdSourcePanel()}>
-              <FiGitMerge />
-              ادغام
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className={css(styles.navItem)}>
-            <Nav.Link eventKey="third" active={activePane === 'ticketing'} href={urls.ticketingPanel()}>
-              <FiMessageSquare />
-              پشتیبانی
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className={css(styles.navItem)}>
-            <Nav.Link eventKey="fourth" active={activePane === 'account'} href={urls.userInfo()}>
-              <FiUser />
-              حساب من
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Col>
-    </Row>
-  </div>
-);
+const MainNavigation = ({ activePane }) => {
+  const mainNavItems = [
+    {
+      text: 'فروشگاه',
+      paneName: 'shop-panel',
+      href: urls.shopPanel(),
+      eventKey: 'first',
+      icon: <FiShoppingCart />,
+    },
+    {
+      text: 'ادغام',
+      paneName: 'crowdsource',
+      href: urls.crowdSourcePanel(),
+      eventKey: 'second',
+      icon: <CrowdSourceIcon />,
+    },
+    {
+      text: 'پشتیبانی',
+      paneName: 'ticketing',
+      href: urls.ticketingPanel(),
+      eventKey: 'third',
+      icon: <FiMessageSquare />,
+    },
+    {
+      text: 'حساب من',
+      paneName: 'account',
+      href: urls.userInfo(),
+      eventKey: 'fourth',
+      icon: <FiUser />,
+    },
+  ];
+
+  return (
+    <div className={css(styles.mainSidebarStick, bgStyles['bg-skylight'])}>
+      <Row>
+        <Col>
+          <Nav variant="pills" className="flex-column">
+            {mainNavItems.map(item => {
+              const active = activePane === item.paneName;
+              return (
+                <Nav.Item className={css(styles.navItem)}>
+                  <Nav.Link
+                    eventKey={item.eventKey}
+                    active={active}
+                    href={item.href}
+                    className={css(styles.navLink, active && styles.activeNavLink)}
+                  >
+                    <FiShoppingCart />
+                    {item.icon}
+                    {item.text}
+                  </Nav.Link>
+                </Nav.Item>
+              );
+            })}
+          </Nav>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 MainNavigation.propTypes = {
   activePane: PropTypes.oneOf(['crowdsource', 'shop-panel', 'ticketing']),
@@ -62,6 +89,13 @@ const styles = StyleSheet.create({
     padding: '12px',
     fontSize: '12px',
     'text-align': 'center',
+  },
+  navLink: {
+    color: colors.blue,
+  },
+  activeNavLink: {
+    color: 'white',
+    backgroundColor: colors.blue,
   },
 });
 
