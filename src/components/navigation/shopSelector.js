873e2b3d7af1bb0072c6a983ebac91a4d4d5c199
AhmadF <fanaei.ahmad@gmail.com>
2020-05-28 14:23:22 +0430

    Make shopSelector fast

--- src/components/navigation/shopSelector.js
+++ src/components/navigation/shopSelector.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Select from 'react-select';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import Select, { createFilter, components } from 'react-select';
+
+const MenuList = props => {
+  const children = React.useMemo(() => React.Children.toArray(props.children), [props.children]);
+
+  return <components.MenuList {...props}>{children.length > 50 ? children.slice(0, 50) : props.children}</components.MenuList>;
+};
+
+const Control = props => {
+  return (
+    <OverlayTrigger placement="bottom" overlay={<Tooltip id="select-help">{props.getValue()[0].label}</Tooltip>}>
+      <div>
+        <components.Control {...props}>{props.children}</components.Control>
+      </div>
+    </OverlayTrigger>
+  );
+};
 
 const StaticShopSelector = ({ isLoading, shops, selectedShop, onChange }) => {
   return (
     <Select
+      filterOption={createFilter({ ignoreAccents: false })}
       isRtl
       placeholder={'انتخاب فروشگاه'}
       noOptionsMessage={() => 'موردی پیدا نشد'}
@@ -14,12 +32,13 @@ const StaticShopSelector = ({ isLoading, shops, selectedShop, onChange }) => {
       isDisabled={isLoading || !shops.length}
       onChange={onChange}
       styles={{ container: styles => ({ ...styles, marginBottom: '1rem' }) }}
+      components={{ MenuList, Control }}
     />
   );
 };
 
 const shopFormat = PropTypes.shape({
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  value: PropTypes.object.isRequired,
   label: PropTypes.string.isRequired,
 });
 StaticShopSelector.propTypes = {
