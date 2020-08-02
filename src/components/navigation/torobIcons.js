1df2bb2332e4621910561c039cdaf3c1817e37d8
AhmadF <fanaei.ahmad@gmail.com>
2020-08-02 17:02:37 +0430

    small fixes and move deps to peer deps #0

--- src/components/navigation/torobIcons.js
+++ src/components/navigation/torobIcons.js
@@ -4,10 +4,10 @@ export const CrowdSourceIcon = ({ style = {}, width = '100%', className = '', vi
   <svg
     stroke="currentColor"
     fill="none"
-    stroke-width="2"
+    strokeWidth="2"
     viewBox="0 0 24 24"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     height="1em"
     width="1.5em"
     xmlns="http://www.w3.org/2000/svg"
